6b5d513489bcd72a27b903a8f32ff9416b14ce30
trungbe <trungnd0601@gmail.com>
2021-03-24 11:07:18 +0700

    tao category service

---
+++ src/main/java/com/module4/casestudy/service/category/ICategoryService.java
@@ -0,0 +1,8 @@
+package com.module4.casestudy.service.category;
+
+import com.module4.casestudy.model.Category;
+import com.module4.casestudy.service.IService;
+
+public interface ICategoryService extends IService<Category> {
+
+}
