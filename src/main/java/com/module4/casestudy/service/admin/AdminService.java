85d07cc144744f3de6c1e2e3459dd3ba5d80ec76
Dat Bong <Datbong2801@>
2021-03-24 10:55:54 +0700

    add repository.admin

--- src/main/java/com/module4/casestudy/service/admin/AdminService.java
+++ src/main/java/com/module4/casestudy/service/admin/AdminService.java
@@ -1,4 +1,24 @@
 package com.module4.casestudy.service.admin;
 
-public class AdminService {
+import com.module4.casestudy.model.LoginUser;
+import com.module4.casestudy.model.UserRole;
+import com.module4.casestudy.repository.admin.IAdminRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AdminService implements IAdminService {
+
+    @Override
+    public Page<LoginUser> findAll(Pageable pageable) {
+        return null;
+    }
+
+    @Override
+    public Page<LoginUser> findUserRoleName(UserRole userRole, Pageable pageable) {
+        return null;
+    }
+
 }
--- src/main/java/com/module4/casestudy/service/admin/IAdminService.java
+++ src/main/java/com/module4/casestudy/service/admin/IAdminService.java
@@ -1,4 +1,11 @@
 package com.module4.casestudy.service.admin;
 
-public class IAdminService {
+import com.module4.casestudy.model.LoginUser;
+import com.module4.casestudy.model.UserRole;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+public interface IAdminService {
+    Page<LoginUser> findAll(Pageable pageable);
+    Page<LoginUser> findUserRoleName(UserRole userRole, Pageable pageable);
 }
