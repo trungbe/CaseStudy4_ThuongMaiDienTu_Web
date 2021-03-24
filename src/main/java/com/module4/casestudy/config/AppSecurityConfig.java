7a6568c22d062e562770575ceac5ad9629f9f75c
Hung <mqhung93@gmail.com>
2021-03-24 15:00:50 +0700

    fix app user

--- src/main/java/com/module4/casestudy/config/AppSecurityConfig.java
+++ src/main/java/com/module4/casestudy/config/AppSecurityConfig.java
@@ -29,7 +29,9 @@ public class AppSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests().antMatchers("/admin/**").hasRole("ADMIN")
                 .and()
-                .authorizeRequests().antMatchers("/user/**").hasRole("USER")
+                .authorizeRequests().antMatchers("/users/**").hasRole("USER")
+                .and()
+                .authorizeRequests().antMatchers("/shop/**").hasRole("SHOP")
                 .and()
                 .formLogin()
                 .and()
