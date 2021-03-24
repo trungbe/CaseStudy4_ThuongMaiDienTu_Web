b47184d279bbacad07958b6aee713486d27f7836
trungbe <trungnd0601@gmail.com>
2021-03-24 10:30:51 +0700

    tao product service

---
+++ src/main/java/com/module4/casestudy/service/product/IProductService.java
@@ -0,0 +1,10 @@
+package com.module4.casestudy.service.product;
+
+
+import com.module4.casestudy.model.Product;
+import com.module4.casestudy.service.IService;
+
+
+public interface IProductService extends IService<Product> {
+
+}
---
+++ src/main/java/com/module4/casestudy/service/product/ProductService.java
@@ -0,0 +1,43 @@
+package com.module4.casestudy.service.product;
+
+import com.module4.casestudy.model.Product;
+import com.module4.casestudy.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProductService implements IProductService {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @Override
+    public List<Product> findALl() {
+        return (List<Product>) productRepository.findAll();
+    }
+
+    @Override
+    public Page<Product> findALl(Pageable pageable) {
+        return productRepository.findAll(pageable);
+    }
+
+    @Override
+    public Product findById(Long id) {
+        return productRepository.findById(id).get();
+    }
+
+    @Override
+    public Product save(Product product) {
+        return productRepository.save(product);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        productRepository.deleteById(id);
+    }
+
+}
