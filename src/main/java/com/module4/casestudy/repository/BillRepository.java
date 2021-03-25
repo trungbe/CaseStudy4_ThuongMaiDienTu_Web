0678fa2f84cece241f461709efe35724044c52d8
Thinh Tran Xuan <you@example.com>
2021-03-25 12:33:52 +0700

    finished backend for cart

--- src/main/java/com/module4/casestudy/repository/BillRepository.java
+++ src/main/java/com/module4/casestudy/repository/BillRepository.java
@@ -12,4 +12,5 @@ public interface BillRepository extends JpaRepository<Bill,Long> {
     @Query(value = "select * from bill where login_user_id = ? and status = false;", nativeQuery = true)
     public List<Bill> findBillNotPayByUserId(Long userId);
 
+
 }
