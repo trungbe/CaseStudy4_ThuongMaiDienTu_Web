e006b22f47f6def4a2a398f7b45714993bd21947
Dat Bong <Datbong2801@>
2021-03-26 14:46:04 +0700

    thêm nút disable

--- src/main/java/com/module4/casestudy/controllers/admin/AdminController.java
+++ src/main/java/com/module4/casestudy/controllers/admin/AdminController.java
@@ -1,16 +1,19 @@
 package com.module4.casestudy.controllers.admin;
 
+import com.module4.casestudy.model.LoginUser;
 import com.module4.casestudy.model.UserRole;
 import com.module4.casestudy.service.admin.AdminService;
 import com.module4.casestudy.service.admin.UserRoleService;
+import com.module4.casestudy.service.loginuser.ILoginUserService;
+import com.module4.casestudy.service.loginuser.LoginUserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -18,6 +21,8 @@ import org.springframework.web.servlet.ModelAndView;
 public class AdminController {
     @Autowired
     private UserRoleService userRoleService;
+    @Autowired
+    private ILoginUserService loginUserService;
 
     @ModelAttribute("userRoles")
     public Iterable<UserRole> userRoles() {
@@ -39,16 +44,29 @@ public class AdminController {
     }
 
     @GetMapping("/list")
-    public ModelAndView showALl(@PageableDefault(size = 10) Pageable pageable) {
+    public ModelAndView showALl(@PageableDefault(size = 7) Pageable pageable) {
         ModelAndView modelAndView = new ModelAndView("admin/adminList");
         modelAndView.addObject("admin", adminService.findAll(pageable));
         return modelAndView;
     }
 
     @PostMapping("/search")
-    public ModelAndView searchByName(@ModelAttribute UserRole userRole, @PageableDefault(size = 10) Pageable pageable) {
+    public ModelAndView searchByName(@ModelAttribute UserRole userRole, @PageableDefault(size = 7) Pageable pageable) {
         ModelAndView modelAndView = new ModelAndView("admin/adminList");
         modelAndView.addObject("admin", adminService.findUserRoleName(userRole, pageable));
         return modelAndView;
     }
+
+    @PutMapping("/list/{id}")
+    private ResponseEntity<LoginUser> disableUser(@PathVariable Long id){
+        LoginUser loginUser = this.loginUserService.findById(id);
+        if (loginUser.isDisable()==true){
+            loginUser.setDisable(false);
+        } else {
+            loginUser.setDisable(true);
+        }
+        loginUserService.save(loginUser);
+        return new ResponseEntity<>(loginUser, HttpStatus.OK);
+    }
+
 }
