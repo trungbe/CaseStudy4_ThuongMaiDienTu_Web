59d9ce67fde26e0a91a1c61529a5ebab01fe0103
Hung <mqhung93@gmail.com>
2021-03-27 09:51:35 +0700

    add tinh nang cho product list

--- src/main/java/com/module4/casestudy/controllers/home/HomeController.java
+++ src/main/java/com/module4/casestudy/controllers/home/HomeController.java
@@ -1,9 +1,8 @@
-package com.module4.casestudy.home;
+package com.module4.casestudy.controllers.home;
 
 import com.module4.casestudy.model.Product;
 import com.module4.casestudy.service.product.IProductService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
